f73424ddd124a68514b5e1e57c303faaedf4c6d8
qqtp2vq9 <qqtp2vq9@gmail.com>
2023-05-18 11:24:15 +0900

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,25 @@
-## env
-set PYTHONPATH "$PYTHONPATH:/usr/local/bin:$HOME/Library/Python/3.9/lib/python/site-packages:$HOME/Library/Python/3.9/bin"
-set GOROOT /usr/local/opt/go/libexec
-set GOPATH $HOME
-set RUBYLIB $RUBYLIB:$RSPEC_RUBYLIB
-set -g fish_user_paths "/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/local/git/bin:$GOPATH/bin:$GOROOT/bin:$PYTHONPATH" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/coreutils/libexec/gnubin:$HOME/.cargo/bin" $fish_user_paths
-set BPCTL_V2_DEFAULT true
+set SDKMAN_DIR "$HOME/.sdkman"
+if test -s "$HOME/.sdkman/bin/sdkman-init.sh"
+  bass source "$HOME/.sdkman/bin/sdkman-init.sh"
+end
 
-## Example aliases
+#### Java
+set -gx JAVA_HOME "$SDKMAN_DIR/candidates/java/current/"
 
+### Example aliases
+alias g='git'
+alias gfp='git fetch --prune'
+alias v='nvim'
+alias sv='sudo nvim'
+alias l='lsd -la'
+
+## コマンド代替え
+alias ls='lsd'
+alias cat='bat'
+alias vim='nvim'
+alias pip='pip3'
+
+## ssh-add
 set -x SSH_AGENT_FILE $HOME/.ssh/ssh-agent
 if test -f $SSH_AGENT_FILE
    source $SSH_AGENT_FILE
@@ -20,8 +31,29 @@ if test $status -ne 0
   #### ssh-agent に -c オプションを追加 (csh-style)
   ssh-agent -c > $SSH_AGENT_FILE
   source $SSH_AGENT_FILE
-  ssh-add $HOME/.ssh/id_rsa
+  sshadd
+end
+
+### fzf.fish
+fzf_configure_bindings --directory=\cf
+
+### ghq.fish
+set -gx GHQ_SELECTOR peco
+
+### path
+set -gx PATH "/opt/homebrew/sbin:/opt/homebrew/bin:$JAVA_HOME/bin:$HOME/.cargo/bin:$HOME/.volta/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:/Library/Apple/usr/bin" PATH
+
+# volta
+set -gx VOLTA_HOME "$HOME/.volta"
+
+### homebrew
+if test -d (brew --prefix)"/share/fish/completions"
+  set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
+end
+
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+  set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
 end
 
 ## starship prompt
-starship init fish | source
+starship init fish | sourcett
